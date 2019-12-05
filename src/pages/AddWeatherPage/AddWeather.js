bf1323e8b997147d5dace1814d358fd282201cf7
dragonza <zell_2004_2005@yahoo.com>
2019-12-04 23:23:49 -0800

    add config

---
+++ src/pages/AddWeatherPage/AddWeather.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import Layout from "../../components/Layout";
+
+class AddWeather extends Component {
+  render() {
+    return (
+      <Layout className="add-weather__container">
+
+      </Layout>
+    );
+  }
+}
+
+AddWeather.propTypes = {};
+
+export default AddWeather;
